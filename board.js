343033ff37e13eb035b803dfd47318943e4a22f2
harukaeru <nekoneko.myaomyao@gmail.com>
2021-02-04 00:48:33 +0900

    Changed State Management

--- board.js
+++ board.js
@@ -1,4 +1,120 @@
 
+// State Management
+const CellState = {
+  ALIVE: 1,
+  DEAD: 0,
+}
+
+const createInitialGeneration = (field) => {
+  const rows = []
+  for (let i = 0; i < field.height; i++) {
+    const row = []
+    rows.push(row)
+    for (let j = 0; j < field.width; j++) {
+      row.push(CellState.DEAD)
+    }
+  }
+  return rows
+}
+
+const getCellState = (generation, columnIndex, rowIndex) => {
+  return generation[rowIndex][columnIndex]
+}
+
+const setCellState = (generation, cellState, columnIndex, rowIndex) => {
+  generation[rowIndex][columnIndex] = cellState
+  renderAliveInBoard(cellState, columnIndex, rowIndex)
+}
+
+// rule
+// xxx
+// xox
+// xxx
+
+const calcNextCellState = (columnIndex, rowIndex, currentGeneration) => {
+  let impossible_index_list = []
+  if (rowIndex === 0) {
+    // 1,2,3は無理
+    impossible_index_list = impossible_index_list.concat([1, 2, 3])
+  }
+  if (columnIndex === 0) {
+    // 1,4,7は無理
+    impossible_index_list = impossible_index_list.concat([1, 4, 7])
+  }
+  if (columnIndex === currentGeneration[0].length - 1) {
+    // 3,6,9は無理
+    impossible_index_list = impossible_index_list.concat([3, 6, 9])
+  }
+  if (rowIndex === currentGeneration.length - 1) {
+    // 7.8,9は無理
+    impossible_index_list = impossible_index_list.concat([7, 8, 9])
+  }
+
+  const impossible_list = Array.from(new Set(impossible_index_list))
+  const neighbors = [1,2,3,4,6,7,8,9].filter(a => !impossible_list.includes(a))
+  const alives = neighbors.map(neighbor => {
+    if (neighbor === 1) {
+      return getCellState(currentGeneration, columnIndex - 1, rowIndex - 1)
+    } else if (neighbor === 2) {
+      return getCellState(currentGeneration, columnIndex, rowIndex - 1)
+    } else if (neighbor === 3) {
+      return getCellState(currentGeneration, columnIndex + 1, rowIndex - 1)
+    } else if (neighbor === 4) {
+      return getCellState(currentGeneration, columnIndex - 1, rowIndex)
+    } else if (neighbor === 6) {
+      return getCellState(currentGeneration, columnIndex + 1, rowIndex)
+    } else if (neighbor === 7) {
+      return getCellState(currentGeneration, columnIndex - 1, rowIndex + 1)
+    } else if (neighbor === 8) {
+      return getCellState(currentGeneration, columnIndex, rowIndex + 1)
+    } else if (neighbor === 9) {
+      return getCellState(currentGeneration, columnIndex + 1, rowIndex + 1)
+    }
+  })
+
+  const aliveCount = alives.filter(alive => alive === CellState.ALIVE).length
+
+  const pos6Alive = getCellState(currentGeneration, columnIndex, rowIndex)
+  if (pos6Alive === CellState.ALIVE) {
+    if (aliveCount == 2 || aliveCount == 3) {
+      return CellState.ALIVE
+    }
+    return CellState.DEAD
+  }
+
+  if (aliveCount === 3) {
+    return CellState.ALIVE
+  }
+
+  return CellState.DEAD
+}
+
+const calcNextGeneration = (currentGeneration) => {
+  const nextGeneration = []
+  const renderingCommands = []
+
+  for (let i = 0; i < currentGeneration.length; i++) {
+    const row = []
+    nextGeneration.push(row)
+    for (let j = 0; j < currentGeneration[i].length; j++) {
+      const currentState = currentGeneration[i][j]
+      const nextState = calcNextCellState(j, i, currentGeneration)
+
+      if (currentState !== nextState) {
+        renderingCommands.push([nextState, i, j])
+      }
+
+      row.push(nextState)
+    }
+  }
+
+  return {
+    nextGeneration,
+    renderingCommands,
+  }
+}
+
+// Renderer
 // field: { width: 30, height: 30 }
 const createBoard = (mountElement, field) => {
   for (let i = 0; i < field.height; i++) {
@@ -12,7 +128,7 @@ const createBoard = (mountElement, field) => {
       cell.className = 'cell'
       cell.setAttribute('data-row', i)
       cell.setAttribute('data-column', j)
-      cell.setAttribute('data-alive', false)
+      cell.setAttribute('data-alive', 0)
       if (field.height - 1 === j) {
         cell.className += ' last-cell'
       }
@@ -24,134 +140,52 @@ const createBoard = (mountElement, field) => {
   }
 }
 
-// set
-const getElem = (columnIndex, rowIndex) => {
-  return document.querySelector(`[data-column="${columnIndex}"][data-row="${rowIndex}"]`)
-}
+const renderAliveInBoard = (isAlive, columnIndex, rowIndex) => {
+  const elem = document.querySelector(`[data-column="${columnIndex}"][data-row="${rowIndex}"]`)
 
-const setAlive = (isAlive, columnIndex, rowIndex) => {
-  const elem = getElem(columnIndex, rowIndex)
   elem.setAttribute('data-alive', isAlive)
 }
 
-const getAlive = (columnIndex, rowIndex) => {
-  const elem = getElem(columnIndex, rowIndex)
+const renderNextField = (currentGeneration) => {
+  const {
+    nextGeneration,
+    renderingCommands,
+  }= calcNextGeneration(currentGeneration)
 
-  return elem.getAttribute('data-alive')
-}
+  for (let i=0; i<renderingCommands.length; i++) {
+    const [cellState, rowIndex, columnIndex] = renderingCommands[i]
+    renderAliveInBoard(cellState, columnIndex, rowIndex)
 
-// rule
-// xxx
-// xox
-// xxx
-
-const calcNext = (columnIndex, rowIndex, field) => {
-  let impossible_index_list = []
-  if (rowIndex === 0) {
-    // 1,2,3は無理
-    impossible_index_list = impossible_index_list.concat([1, 2, 3])
   }
-  if (columnIndex === 0) {
-    // 1,4,7は無理
-    impossible_index_list = impossible_index_list.concat([1, 4, 7])
-  }
-  if (columnIndex === field.width - 1) {
-    // 3,6,9は無理
-    impossible_index_list = impossible_index_list.concat([3, 6, 9])
-  }
-  if (rowIndex === field.height - 1) {
-    // 7.8,9は無理
-    impossible_index_list = impossible_index_list.concat([7, 8, 9])
-  }
-
-  const impossible_list = Array.from(new Set(impossible_index_list))
-  const neighbors = [1,2,3,4,6,7,8,9].filter(a => !impossible_list.includes(a))
-  const alives = neighbors.map(neighbor => {
-    if (neighbor === 1) {
-      return getAlive(columnIndex - 1, rowIndex - 1)
-    } else if (neighbor === 2) {
-      return getAlive(columnIndex, rowIndex - 1)
-    } else if (neighbor === 3) {
-      return getAlive(columnIndex + 1, rowIndex - 1)
-    } else if (neighbor === 4) {
-      return getAlive(columnIndex - 1, rowIndex)
-    } else if (neighbor === 6) {
-      return getAlive(columnIndex + 1, rowIndex)
-    } else if (neighbor === 7) {
-      return getAlive(columnIndex - 1, rowIndex + 1)
-    } else if (neighbor === 8) {
-      return getAlive(columnIndex, rowIndex + 1)
-    } else if (neighbor === 9) {
-      return getAlive(columnIndex + 1, rowIndex + 1)
-    }
-  })
-
-  const aliveCount = alives.filter(alive => alive === 'true').length
-
-  const pos6Alive = getAlive(columnIndex, rowIndex)
-  if (pos6Alive === 'true') {
-    if (aliveCount == 2 || aliveCount == 3) {
-      return true
-    }
-    return false
-  }
-
-  if (aliveCount === 3) {
-    return true
-  }
-
-  return false
-}
-
-const calc = (field) => {
-  const nextGeneration = []
-
-  for (let i = 0; i < field.height; i++) {
-    const row = []
-    nextGeneration.push(row)
-    for (let j = 0; j < field.width; j++) {
-      row.push(calcNext(j, i, field))
-    }
-  }
-
   return nextGeneration
 }
 
-const render = (data) => {
-  for (let i = 0; i < data.length; i++) {
-    for (let j = 0; j < data[i].length; j++) {
-      setAlive(data[i][j], j, i)
-    }
-  }
-}
-
-const next = () => {
-  const data = calc({ width: 20, height: 20 })
-  render(data)
-}
-
-const initialize = () => {
-  createBoard(document.getElementById('root'), { width: 20, height: 20 })
-}
-
-initialize()
-
-setAlive(true, 1, 0)
-setAlive(true, 2, 1)
-setAlive(true, 0, 2)
-setAlive(true, 1, 2)
-setAlive(true, 2, 2)
-
-const nnext = (count) => {
+// Game Manager
+const nnext = (count, generation) => {
   if (count === 0) {
     return
   }
 
   setTimeout(() => {
-    next()
-    nnext(count - 1)
-  }, 300)
+    const nextGeneration = renderNextField(generation)
+    nnext(count - 1, nextGeneration)
+  }, 150)
 }
 
+// __main__
+const initialize = (field) => {
+  const initialGeneration = createInitialGeneration(field)
+  createBoard(document.getElementById('root'), field)
 
-nnext(100)
+  setCellState(initialGeneration, CellState.ALIVE, 1, 0)
+  setCellState(initialGeneration, CellState.ALIVE, 1, 2)
+  setCellState(initialGeneration, CellState.ALIVE, 2, 1)
+  setCellState(initialGeneration, CellState.ALIVE, 0, 2)
+  setCellState(initialGeneration, CellState.ALIVE, 1, 2)
+  setCellState(initialGeneration, CellState.ALIVE, 2, 2)
+
+  nnext(1000, initialGeneration)
+}
+
+const field = { width: 50, height: 50 }
+initialize(field)
